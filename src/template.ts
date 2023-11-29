f2551d68d613ada45aff7bca175aa1d4d9607657
peron <peron_ap@icloud.com>
2023-11-29 23:19:26 +0000

    feat: support dynamic routing

--- src/template.ts
+++ src/template.ts
@@ -3,6 +3,9 @@ import { ejs, path } from "./deps.ts";
 export async function render(
   { templatePath, data, root }: RenderOptions,
 ): Promise<string | undefined> {
+  // clone data to prevent mutation
+  data = { ...data };
+
   const jointPath = path.join(root, templatePath);
   if (!jointPath.startsWith(root)) return;
 
@@ -10,12 +13,19 @@ export async function render(
 
   try {
     const { isDirectory } = await Deno.stat(filepath);
-    if (isDirectory) filepath = path.join(filepath, "index");
+    if (isDirectory) filepath = path.join(filepath, "index.ejs");
   } catch {
     // do nothing
   }
 
-  if (!filepath.endsWith(".ejs")) filepath += ".ejs";
+  const lastEjsEndIndex = filepath.lastIndexOf(".ejs") + ".ejs".length;
+  if (lastEjsEndIndex !== filepath.length) {
+    // handle dynamic route
+    data.routeParam = filepath.slice(lastEjsEndIndex);
+    filepath = filepath.slice(0, lastEjsEndIndex);
+  } else {
+    data.routeParam = undefined;
+  }
 
   try {
     return await ejs.renderFile(filepath, data, { async: true });
